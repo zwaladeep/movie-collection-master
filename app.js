52e581648445bd707ce182be4cb4edb32c6bfb6e
zwaladeep <zwaladeep.sonapuram@gmail.com>
2019-03-25 09:12:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var express = require('express');
+var app = express();
+module.exports = app; // for testing
+app.use('/swagger',express.static('\api\\swagger'));
+var db = require('./config/db')();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+});
